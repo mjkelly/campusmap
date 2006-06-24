51491c08e61fd5c4b5c738a04e1e8570af466c77
dlindqui <dlindqui@8b0ed335-5601-0410-b8dd-ab62b34c5d12>
2006-06-24 04:34:34 +0000

    Links to about

--- cgi-bin/lib/MapGlobals.pm
+++ cgi-bin/lib/MapGlobals.pm
@@ -118,8 +118,8 @@ our %TEMPLATES = (
 );
 
 # where static content is stored
-our $HTML_BASE		= '../../ucsdmap';		# url
-our $HTML_PATH		= '../../ucsdmap';		# path
+our $HTML_BASE		= '../../campusmap';		# url
+our $HTML_PATH		= '../../campusmap';		# path
 ##our $HTML_BASE		= '.';					# url
 ##our $HTML_PATH		= '/home/mkelly/web/public/campusmap';	# path
 

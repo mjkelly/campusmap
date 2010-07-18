8c1e008a2515b4f5168ad330d999d5f7f4b45612
Michael Kelly <michael@michaelkelly.org>
2010-07-18 17:25:56 -0400

    Use template in map_handlers.py. Template unchanged.

--- campusmap/map_handlers.py
+++ campusmap/map_handlers.py
@@ -14,17 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+import os
 import urllib
 
 class ViewHandler(webapp.RequestHandler):
     def get(self, _unused):
-        self.response.out.write("""
-<h1>CampusMap</h1>
-<p>Map.</p>
-""")
+        template_values = {
+            'greeting': 'Hello!'}
+       path = os.path.join(os.path.dirname(__file__), 'js_tmpl.html')
+        self.response.out.write(template.render(path, template_values))
 
     def post(self):
-        self.response.out.write("""ok""")
+        self.response.out.write('ok')
 

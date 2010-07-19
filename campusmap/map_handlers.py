a9300a31ccb784cb8e43d0c7c1ded96aa2ae788d
Michael Kelly <michael@michaelkelly.org>
2010-07-19 01:55:58 -0400

    Clarify some template values.

--- campusmap/map_handlers.py
+++ campusmap/map_handlers.py
@@ -35,10 +35,12 @@ class ViewHandler(webapp.RequestHandler):
 
         default_xoff  = 4608
         default_yoff  = 3172
-        # is the difference between these?
-        default_scale = 3
-        default_size = 1
 
+        # default zoom level
+        default_scale = 3
+        # default viewport size (index into old defaults; not currently used)
+        default_size = 1
+        # default walk sped (minutes per mile)
         default_mpm = 20
 
         html_base = '/static'
@@ -47,21 +49,6 @@ class ViewHandler(webapp.RequestHandler):
         locations = fh.read()
         fh.close()
 
-        # # subdirectories of $HTML base for specific types of data
-        # CSS_DIR         = $HTML_BASE . '/css'
-        # STATIC_IMG_DIR  = $HTML_BASE . '/images'
-        # DYNAMIC_IMG_DIR = $HTML_BASE . '/dynamic'
-        # DYNAMIC_IMG_PATH        = $HTML_PATH . '/dynamic'
-        # PATH_IMG_DIR    = $DYNAMIC_IMG_DIR . '/paths'
-        # PATH_IMG_PATH   = $DYNAMIC_IMG_PATH . '/paths'
-        # GRID_IMG_DIR    = $STATIC_IMG_DIR . '/grid'
-        #
-        # # where binary input files are stored
-        # DATA_DIR                = 'data'
-        # # locations of the binary files that contain the graph of paths
-        # POINT_FILE              = $DATA_DIR . '/binPointData.dat'
-        # LOCATION_FILE   = $DATA_DIR . '/binLocationData.dat'
-        # EDGE_FILE               = $DATA_DIR . '/binEdgeData.dat'
         template_values = {
             'html_dir': html_base,
             'css_dir': html_base + '/css',
@@ -86,7 +73,6 @@ class ViewHandler(webapp.RequestHandler):
             'xoff' : default_xoff,
             'yoff' : default_yoff,
 
-            # TODO: what _are_ these?
             'width': 500,
             'height': 375,
             'scale': default_scale,

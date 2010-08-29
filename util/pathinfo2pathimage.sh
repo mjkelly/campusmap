5657d60bd727054af9c3f8fca8a87d2921ad7d94
Michael Kelly <michael@michaelkelly.org>
2010-08-28 20:41:13 -0400

    Remove sample-data directory, which contained misleading data. Update pathinfo2pathimage.sh to get the format right.

--- util/pathinfo2pathimage.sh
+++ util/pathinfo2pathimage.sh
@@ -9,4 +9,4 @@ IN=pathinfo.csv
 OUT=pathimage.csv
 
 echo 'id0,id1,zoom' > "$OUT"
-tail -n +2 "$IN" | perl -p -e '@l = split(/,/); $_ = "$l[0],$l[1],0\n$l[0],$l[1],1\n$l[0],$l[1],2\n$l[0],$l[1],3\n";' >> "$OUT"
+tail -n +2 "$IN" | perl -p -e '@l = split(/,/); $_ = "$l[1],$l[2],0\n$l[1],$l[2],1\n$l[1],$l[2],2\n$l[1],$l[2],3\n";' >> "$OUT"
